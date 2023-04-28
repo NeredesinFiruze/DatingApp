269588396a169b0962222280f94cafe5f4ddd1e9
NeredesinFiruze <nahya5601@gmail.com>
2023-04-28 18:43:47 +0300

    change enter condition

--- app/src/main/java/com/example/datingapp/presentation/SplashScreen.kt
+++ app/src/main/java/com/example/datingapp/presentation/SplashScreen.kt
@@ -15,19 +15,21 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
-import com.example.datingapp.presentation.home.HomeViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.delay
 import kotlin.time.Duration.Companion.seconds
 
 @Composable
 fun SplashScreen(navController: NavController) {
 
-    val homeViewModel = hiltViewModel<HomeViewModel>()
-    val signIn by homeViewModel.signInComplete.value.collectAsStateWithLifecycle(false)
+    //val homeViewModel = hiltViewModel<HomeViewModel>()
+    //val signIn by homeViewModel.signInComplete.value.collectAsStateWithLifecycle(false)
+    val userId = FirebaseAuth.getInstance().currentUser?.uid
+    val ref = FirebaseDatabase.getInstance().getReference("users")
+    val isSignedInBefore = ref.child(userId.toString()).key != null
+
 
     var value by remember { mutableStateOf(12) }
     val size by animateIntAsState(
@@ -38,8 +40,8 @@ fun SplashScreen(navController: NavController) {
     LaunchedEffect(Unit) {
         value = 32
         delay(1.seconds)
-        val destination = if (FirebaseAuth.getInstance().currentUser == null) "sign-in"
-                          else if (signIn == true) "home"
+        val destination = if (userId == null) "sign-in"
+                          else if (isSignedInBefore) "home"
                           else "on-boarding"
 
         navController.navigate(destination)
