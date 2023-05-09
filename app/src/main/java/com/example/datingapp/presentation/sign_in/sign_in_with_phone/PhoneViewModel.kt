bcd77a144680d2a82306d667797da6a4395f5195
NeredesinFiruze <nahya5601@gmail.com>
2023-05-09 17:59:06 +0300

    sign in "check next destination" function added

--- app/src/main/java/com/example/datingapp/presentation/sign_in/sign_in_with_phone/PhoneViewModel.kt
+++ app/src/main/java/com/example/datingapp/presentation/sign_in/sign_in_with_phone/PhoneViewModel.kt
@@ -1,17 +1,25 @@
-package com.example.datingapp.presentation.sign_in_screen.sign_in_with_phone
+package com.example.datingapp.presentation.sign_in.sign_in_with_phone
 
 import android.app.Activity
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.datingapp.navigation.Screen
 import com.example.datingapp.util.CountryISO
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.coroutineScope
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
@@ -51,7 +59,7 @@ class PhoneViewModel @Inject constructor() : ViewModel() {
         }
         val options = PhoneAuthOptions.newBuilder(auth)
             .setPhoneNumber(phoneNumber)
-            .setTimeout(60L, TimeUnit.SECONDS)
+            .setTimeout(0L, TimeUnit.SECONDS)
             .setActivity(context as Activity)
             .setCallbacks(callbacks)
             .build()
@@ -70,5 +78,26 @@ class PhoneViewModel @Inject constructor() : ViewModel() {
                     Toast.makeText(context, "fail", Toast.LENGTH_LONG).show()
                 }
             }
+    }
+
+    suspend fun destination(): String = coroutineScope {
+        val userId = FirebaseAuth.getInstance().currentUser?.uid!!
+        val path = Firebase.database.getReference("users")
+            .child(userId)
+            .child("connectionStatus")
+
+        val deferred = CompletableDeferred<String>()
+
+        path.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val exists = snapshot.exists()
+                val destination = if (exists) Screen.Home.route else Screen.OnBoarding.route
+                deferred.complete(destination)
+            }
+
+            override fun onCancelled(error: DatabaseError) = Unit
+        })
+
+        deferred.await()
     }
 }
